de68791879159f659a16247406f13432d0c17df7
mangotree11 <71046602+mangotree11@users.noreply.github.com>
2020-09-23 20:12:53 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -31,7 +31,29 @@ class Calculator {
    }
 
    compute() {
-   
+      let computation
+      const prev = parseFloat(this.previousOperand)
+      const current = parseFloat(this.currentOperand)
+      if (isNaN(prev) || isNaN(current)) return
+      switch (this.operation) {
+         case '+':
+            computation = prev + current
+            break
+         case '-':
+            computation = prev - current
+            break
+         case '*':
+            computation = prev * current
+            break
+         case '÷':
+            computation = prev / current
+            break
+         default:
+            return
+      }
+      this.currentOperand = computation
+      this.operation = undefined
+      this.previousOperand = ''
    }
 
 updateDisplay() {
@@ -69,3 +91,13 @@ equalsButton.addEventListener('click', button => {
    calculator.compute()
    calculator.updateDisplay()
 })
+
+allClearButton.addEventListener('click', button => {
+   calculator.clear()
+   calculator.updateDisplay()
+ })
+
+ deleteButton.addEventListener('click', button => {
+   calculator.delete()
+   calculator.updateDisplay()
+ })
